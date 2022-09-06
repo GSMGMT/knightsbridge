fafa578e491130cb5ae4c2745e51205c495c3335
João Vítor Alves <joao.alves@codeminer42.com>
2022-09-06 15:21:17 -0300

    feat(crypto): add market pair fetch by name function

---
+++ libs/firebase/functions/marketPair/getMarketPairByName.ts
@@ -0,0 +1,19 @@
+import { MarketPair } from '@contracts/MarketPair';
+import { firestore } from '@libs/firebase/admin-config';
+import { FirebaseCollections } from '@libs/firebase/collections';
+import { MarketPairConverter } from '@libs/firebase/converters/marketPairConverter';
+
+const getMarketPairByName = async (
+  marketPairName: string
+): Promise<MarketPair | null> => {
+  const MarketPairCollection = firestore()
+    .collection(FirebaseCollections.MARKET_PAIR)
+    .where('name', '==', marketPairName)
+    .withConverter(MarketPairConverter);
+
+  const querySnapshot = await MarketPairCollection.get();
+
+  return !querySnapshot.empty ? querySnapshot.docs[0].data() : null;
+};
+
+export default getMarketPairByName;
