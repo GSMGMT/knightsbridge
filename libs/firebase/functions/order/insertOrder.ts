a042eeb86b1235868d08edea5935f6cb96e975f4
João Vítor Alves <joao.alves@codeminer42.com>
2022-09-08 14:08:18 -0300

    feat(order): add order insertion function

---
+++ libs/firebase/functions/order/insertOrder.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { OrderStatus } from '@contracts/Order';
+import { User } from '@contracts/User';
+import { MarketPair } from '@contracts/MarketPair';
+import { FirebaseCollections } from '@libs/firebase/collections';
+import { firestore } from '@libs/firebase/admin-config';
+import { OrderConverter } from '@libs/firebase/converters/orderConverter';
+import { OmitTimestamp } from '@utils/types';
+
+interface InsertOrder {
+  type: 'buy' | 'sell';
+  user: OmitTimestamp<User>;
+  marketPair: OmitTimestamp<MarketPair>;
+  price: number;
+  amount: number;
+  total: number;
+  fee: number;
+}
+
+const insertOrder = async (walletUid: string, newOrder: InsertOrder) => {
+  const uid = uuidv4();
+  const serverTime = firestore.FieldValue.serverTimestamp();
+
+  const OrderDoc = firestore()
+    .collection(FirebaseCollections.WALLETS)
+    .doc(walletUid)
+    .withConverter(OrderConverter);
+
+  await OrderDoc.set({
+    uid,
+    ...newOrder,
+    status: OrderStatus.PROCESSING,
+    createdAt: serverTime,
+    updatedAt: serverTime,
+  });
+};
+
+export default insertOrder;
