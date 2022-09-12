aa37f188f0cfe5f2bb522962afc9748ea36fe3d4
souzadaniel <eu@danielsouza.dev.br>
2022-09-12 09:49:32 -0300

    fix(component): fixing domain displayed

--- components/Security/index.tsx
+++ components/Security/index.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import { useEffect, useMemo, useState } from 'react';
+import { useSsr } from 'usehooks-ts';
 
 import styles from './Security.module.scss';
 
@@ -7,13 +9,26 @@ import { Icon } from '../Icon';
 export const Security = () => {
   const { pathname } = useRouter();
 
+  const [domain, setDomain] = useState<string>('');
+  const [protocol, setProtocol] = useState<string>('');
+  const protocolAccess = useMemo(() => `${protocol}//`, [protocol]);
+
+  const { isBrowser } = useSsr();
+
+  useEffect(() => {
+    if (isBrowser) {
+      setDomain(window.location.host);
+      setProtocol(window.location.protocol);
+    }
+  }, [isBrowser]);
+
   return (
     <div className={styles.correct}>
       <Icon name="lock" size={24} />
       <div className={styles.url}>
-        https://
+        {protocolAccess}
         <span>
-          knights.app
+          {domain}
           {pathname}
         </span>
       </div>
